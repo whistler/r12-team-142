7d18de3328b97a582f42a0c8648972bf798c5ca1
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-10-13 12:00:33 -0400

    added client side markdown generator

--- app/assets/javascripts/canvases.js
+++ app/assets/javascripts/canvases.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
+
+  var converter = new Showdown.converter();
+
+  $('.best_in_place').bind("ajax:success", function(){
+    var new_html = converter.makeHtml($(this).html());
+    $(this).html(new_html);
+  });
+
 });
