713c05b3855ab5e4f83e58a730070431fe536445
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-10-12 20:29:01 -0400

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeanWall::Application.config.secret_token = 'c086d579b41cba0ddd32c9f0114d8a8f9eefdae6a34acfdfd9bd6e95ac9e4a2036fc8070e29d3069f84d9a8e13bd8df248b674ad203bbcce269922e3fc93dcaa'
