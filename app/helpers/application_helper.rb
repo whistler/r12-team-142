ae015ce56228ea38b30ef17cd6e80c417e57d3f2
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-10-14 09:28:16 -0400

    created a header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def show_header?
+    if params[:controller] == "welcome" and params[:action] == "index"
+      return false
+    elsif params[:controller] == "canvases" and params[:action] == "show"
+      return false
+    else
+      return true
+    end
+  end
 end
